7e8c0e636061c415d7a3eed921915ab6364784a3
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-05 17:16:37 -0300

    Adiciona atividade Cebolinha

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.Write("Digite aqui a frase para o Cebolinha repetir: ");
+            text = Console.ReadLine();
+
+            // ---------------------
+
+            Console.WriteLine("O Cebolinha diz: {0}",text.Replace("r","l").Replace("R","L"));
+        }
+    }
+}
